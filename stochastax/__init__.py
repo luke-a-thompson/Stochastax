66b3e54a22447441aa53c6d03af8e6143f55e50b
Luke Thompson <luke.thompson@sydney.edu.au>
2025-11-27 16:29:03 +1100

    Feat: Library init docs

--- stochastax/__init__.py
+++ stochastax/__init__.py
@@ -1,6 +1,27 @@
-"""Quicksig: Quicksig is a library for rough path computations."""
+"""Stochastax: A Jax library for advanced stochastic analysis.
 
-__version__ = "0.1.4"
+## Features
+
+### Rough Paths
+- Path signatures
+- Log signatures
+- Branched signatures (MKW, BCK)
+- Log-ODE
+- Rough path Hopf algebras
+- Path augmentations (time, lead-lag, non-overlapping windows)
+- Misc analytic tools (Hurst index, Wong-Zakai step count, Friz-Riedel meshsize)
+
+### Butcher Series
+- Butcher series
+- Lie-Butcher series
+
+### Stochastic and Rough Differential Equations
+- Brownian motion
+- Fractional Brownian motion
+- Riemann-Liouville (Type II) fractional Brownian motion
+"""
+
+__version__ = "0.4.1"
 __author__ = "Luke Thompson"
 __email__ = "luke.thompson@sydney.edu.au"
 __license__ = "MIT"
--- stochastax/controls/__init__.py
+++ stochastax/controls/__init__.py
@@ -1,3 +1,28 @@
+"""
+Controls for stochastic and rough differential equations.
+
+## Public API
+### Paths:
+- Path: A path in a state space.
+- pathify: Convert a JAX array of a data stream into a Path object.
+
+### Drivers:
+- bm_driver: Generate a Brownian motion path.
+- correlate_bm_driver_against_reference: Correlate a Brownian motion path against a reference path.
+- correlated_bm_drivers: Generate a new Brownian motion path that is correlated with a reference path.
+- riemann_liouville_driver: Generate a Riemann-Liouville fractional Brownian motion path.
+- fractional_bm_driver: Generate a fractional Brownian motion path.
+
+### Augmentations:
+- augment_path: Augment a path with a list of augmentations.
+- basepoint_augmentation: Augment a path with a basepoint.
+- time_augmentation: Augment a path with a time shift.
+- lead_lag_augmentation: Augment a path with a lead-lag shift.
+- non_overlapping_windower: Augment a path with a non-overlapping window.
+- dyadic_windower: Augment a path with a dyadic windower.
+
+"""
+
 from .paths_types import Path, pathify
 from .augmentations import (
     augment_path,
@@ -30,6 +55,3 @@ __all__ = [
     "riemann_liouville_driver",
     "fractional_bm_driver",
 ]
-
-
-
